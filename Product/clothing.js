4560ba3e0a91c3de7bd162614267fd656f665600
Shreyashnaveenmasai <shreyashnaveen@hotmail.com>
2022-12-04 22:29:47 +0530

    product

---
+++ Product/clothing.js
@@ -0,0 +1,15 @@
+var clothing = [
+    {
+        id: 1,
+        image_url:"https://cdn.shopify.com/s/files/1/0248/3473/6191/products/aap9VKrZwOff2EzzMvDWfugXcJhbPyfF-25_large.jpg?v=1669532929",
+        brand:"BCBMAXAZRIA"
+        name: "CROPPED LIQUID PUFFER WOMENS METALLIC CROPPED PUFFER COAT"
+        price : "$92.99"
+    }
+    {
+        id:2,
+        image_url:"https://cdn.shopify.com/s/files/1/0248/3473/6191/products/83728-83730_b-X3_large.jpg?v=1669794585",
+        brand: "REAL CASHMERE"
+        name:"REAL CASHMERE DARK BLUE HALF ZIP FINE CASHMERE BLEND MENS SWEATER"
+    }
+]
